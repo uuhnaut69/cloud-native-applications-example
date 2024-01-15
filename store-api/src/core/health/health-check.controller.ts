bce4e3ab372278c20a49f2e6b3ed72e8d4f8633d
uuhnaut69 <uuhnaut96@gmail.com>
2024-01-15 16:42:56 +0700

    feat: Add health check

--- store-api/src/core/health/health-check.controller.ts
+++ store-api/src/core/health/health-check.controller.ts
@@ -1,6 +1,7 @@
 import { BaseApiResponse } from '@app/common/decorators/base-api-response.decorator';
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
 
 @ApiTags('Health Check')
 @Controller({
@@ -8,14 +9,23 @@ import { ApiTags } from '@nestjs/swagger';
   version: '1',
 })
 export class HealthCheckController {
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private readonly db: TypeOrmHealthIndicator,
+  ) {}
+
   @BaseApiResponse({
     summary: 'Get service health',
     schemaType: 'string',
     refType: String,
-    example: 'Up',
+    example: 'ok',
   })
   @Get()
-  public getServiceHealth() {
-    return 'Up';
+  public async getServiceHealth() {
+    const results = await this.healthCheckService.check([
+      () => this.db.pingCheck('database'),
+    ]);
+
+    return results.status?.toUpperCase();
   }
 }
--- store-api/src/core/health/health.module.ts
+++ store-api/src/core/health/health.module.ts
@@ -1,7 +1,9 @@
 import { HealthCheckController } from '@app/core/health/health-check.controller';
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
+  imports: [TerminusModule],
   controllers: [HealthCheckController],
 })
 export class HealthModule {}
